dbfbada91443f5f9eff68ec60d9ee9f3d27c5738
HaibaoM55 <96226102+HaibaoM55@users.noreply.github.com>
2024-12-01 08:30:42 +0200

    1 decembrie 🇷🇴

--- teatru/main.cpp
+++ teatru/main.cpp
@@ -7,25 +7,31 @@ char x[55005];
 int nr = 0;
 int nrmax=0;
 int nri, nrj;
-bool f[26];
+int f[26];
 int main()
 {
     fin >> n >> k;
     for(int i = 1; i <= n; i++){
         fin >> x[i];
+
     }
     nr = 0;
     int i = 1;
     for(int j = 1; j <= n; j++){
-        if(f[x[j]-'A'] == false){
+        if(f[x[j]-'A'] == 0){
             nr++;
-            f[x[j]-'A']=true;
-            if(nr > k){
-                nr--;
+            f[x[j]-'A']++;
+            while(nr > k){
+                f[x[i]-'A']--;
+                if(f[x[i]-'A'] == 0){
+                    nr--;
+                }
                 i++;
             }
+        }else{
+            f[x[j]-'A']++;
         }
-        if(j-i+1 > nrmax){
+        if(nr <= k && j-i+1 > nrmax){
             nrmax = j-i+1;
             nri = i;
             nrj = j;
