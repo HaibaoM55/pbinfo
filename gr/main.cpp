d13255e167bfc1792e2821d79bbf52b96a8004b2
HaibaoM55 <96226102+HaibaoM55@users.noreply.github.com>
2025-02-03 20:22:35 +0200

    3 februarie

--- gr/main.cpp
+++ gr/main.cpp
@@ -14,6 +14,34 @@ int main()
     for(int i = 1; i <= n; i++){
         fin >> b[i];
     }
-
+    int vmax = 0;
+    int r=0;
+    int c=0;
+    for(int i=1; i <= n;i++){
+        if(a[i] == c){
+            c = 0;
+        }else if(a[i] < c){
+            r = max(r,a[i]);
+        }else{
+            r = max(r,c);
+            c = a[i];
+        }
+    }
+    vmax = max(r, c);
+    r=0;
+    c=0;
+    for(int i=1; i <= n;i++){
+        if(b[i] == c){
+            c = 0;
+        }else if(b[i] < c){
+            r = max(r,b[i]);
+        }else{
+            r = max(r,c);
+            c = b[i];
+        }
+    }
+    vmax = max(vmax, r);
+    vmax = max(vmax, c);
+    fout << vmax;
     return 0;
 }
