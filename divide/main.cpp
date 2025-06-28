9a5bfd66a16c84e22cd9c2039f7d36c696e60ae0
HaibaoM55 <raulbogdan559@gmail.com>
2025-06-28 15:04:33 +0300

    28 iunie

--- divide/main.cpp
+++ divide/main.cpp
@@ -1,28 +1,29 @@
+///sunt parcurse toate submultimile, se porneste de la submultimea vida cu suma 0
+///in a[] se pastreaza sumele tuturor submultimilor, cel mult 2^20
+///aceasta forma de parcurgere poate fi utilizata si pentru tehnica PINEX (principiul includeri/excluderii)
 #include <iostream>
 #include <algorithm>
 using namespace std;
 int n;
-int v[20],a[20];
+int v[22],na;
+long long a[2000002],s,s1;
+
 int main(){
     cin >> n;
-    long long s =0;
     for(int i = 1; i <= n; i++){
-        a[i] = i;
         cin >> v[i];
         s = s + v[i];
     }
-    long long mindif=s;
-    do{
-        long long s1 =0;
-        for(int i = 1; i <= n; i++){
-            if(s1+v[a[i]] > s/2){
-                break;
-            }else{
-                s1 = s1+v[a[i]];
+    na=0;
+    a[0]=0;
+    for(int i=1;i<=n;i++){
+        for(int j=na;j>=0;j--){
+            a[++na]=a[j]+v[i];
+            if(a[na]<=s/2 && a[na]>s1){
+                s1=a[na];
             }
         }
-        mindif = min(mindif, s-2*s1);
-    }while(next_permutation(a+1, a+n+1));
-    cout << mindif;
+    }
+    cout << s-s1-s1;
     return 0;
 }
