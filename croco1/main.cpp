8fe5ca6496fb28678196225d597c68d9c20c7c99
HaibaoM55 <raulbogdan559@gmail.com>
2025-10-10 19:05:38 +0300

    10 octombrie 2025

--- croco1/main.cpp
+++ croco1/main.cpp
@@ -1,51 +1,38 @@
 #include <fstream>
 using namespace std;
-ifstream fin("croco1.in");
+int n,m,a[102][102],nc=0,ne=0;
+ifstream  fin("croco1.in");
 ofstream fout("croco1.out");
-int n, m;
-int a[104][104];
-int dirx[6] = {1,-1,1,-1};
-int diry[6] = {1,-1,-1,1};
-int leefill(int i, int j, int v[104][104]){
-    if(v[i][j] == 0){
-        v[i][j] = 3; // crocodil
-        int nr = 1;
-        for(int p = 0; p < 4; p++){
-            int i1 = i+dirx[p];
-            int j1 = j+diry[p];
-            if(1 <= i1 && i1 <= n){
-                if(1 <= j1 && j1 <= m){
-                    nr += leefill(i1, j1, v);
-                }
-            }
-        }
-        return nr;
-    }else{
-        return 0;
+void fill(int lin, int col, int &n2, int &n3, int u){
+    if(lin>=1 && lin<=n && col>=1 && col<=m && a[lin][col]==0){
+        a[lin][col]=5-u;
+        if(5-u==2)n2++;
+        else n3++;
+        fill(lin-1,col,n2,n3,5-u);
+        fill(lin+1,col,n2,n3,5-u);
+        fill(lin,col-1,n2,n3,5-u);
+        fill(lin,col+1,n2,n3,5-u);
     }
 }
+
 int main(){
-    fin >> n >> m;
-    int nra = 0;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
-            fin >> a[i][j];
-            if(a[i][j] == 1){
-                nra++;
+    fin>>n>>m;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            fin>>a[i][j];
+        }
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(a[i][j]==0){
+                int n2=0,n3=0;
+                fill(i,j,n2,n3,3);
+                if(n2<n3)swap(n2,n3);
+                nc+=n2;
+                ne+=n3;
             }
         }
     }
-    int vmax = 0;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
-            if(a[i][j] == 0){
-                int x = leefill(i,j, a);
-                if(x > vmax){
-                    vmax = x;
-                }
-            }
-        }
-    }
-    fout << vmax << ' ' << n*m-vmax-nra;
+    fout<<nc<<" "<<ne;
     return 0;
 }
