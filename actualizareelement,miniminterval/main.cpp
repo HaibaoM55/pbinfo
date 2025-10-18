16a71fac227ff5e970c21a17da9619b18f51aa8c
HaibaoM55 <raulbogdan559@gmail.com>
2025-10-18 15:51:48 +0300

    18 octombrie 2025

--- actualizareelement,miniminterval/main.cpp
+++ actualizareelement,miniminterval/main.cpp
@@ -1,79 +1,70 @@
-#include <fstream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-ifstream  fin("aemi.in");
+ifstream fin("aemi.in");
 ofstream fout("aemi.out");
-int n,Q,v[100003],vmin[400];
+int n, m, t, a, b, v[100005];
 
-int main(){
-    fin>>n;
-    for(int i=1;i<=n;i++){
-        fin>>v[i];
+struct _Heap {
+  _Heap *right_child, *left_child, *parent;
+  int value;
+};
+
+int get_best_child(_Heap *child_left, _Heap *child_right) {
+  return min(child_left->value, child_right->value);
+}
+void init_heap(_Heap *Node, int left, int right) {
+  if(left == right)
+    Node->value = v[left];
+  else {
+    int mid = (left + right) / 2;
+    _Heap *new_child_left = new _Heap();
+    _Heap *new_child_right = new _Heap();
+    Node->left_child = new_child_left;
+    Node->right_child = new_child_right;
+    init_heap(Node->left_child, left, mid);
+    init_heap(Node->right_child, mid + 1, right);
+    Node->value = get_best_child(Node->left_child, Node->right_child);
+  }
+}
+void update_heap(_Heap *Node, int left, int right, int pos_query, int value_query) {
+  if(left == pos_query && right == pos_query) {
+    Node->value = value_query;
+  } else {
+    int mid = (left + right) / 2;
+    if(pos_query <= mid)
+      update_heap(Node->left_child, left, mid, pos_query, value_query);
+    else
+      update_heap(Node->right_child, mid + 1, right, pos_query, value_query);
+    Node->value = get_best_child(Node->left_child, Node->right_child);
+  }
+}
+int query_heap(_Heap *Node, int left, int right, int query_left, int query_right) {
+  if((query_left <= left  && right <= query_right))
+    return Node->value;
+  else {
+    int mini = INT_MAX, mid = (left + right) / 2;
+    if(query_left <= mid)
+      mini = min(mini, query_heap(Node->left_child, left, mid, query_left, query_right));
+    if(query_right > mid)
+      mini = min(mini, query_heap(Node->right_child, mid + 1, right, query_left, query_right));
+    return mini;
+  }
+}
+int main() {
+  fin >> n;
+  for(int i = 1; i <= n; i++)
+    fin >> v[i];
+  _Heap *root = new _Heap();
+  root->parent = NULL;
+  init_heap(root, 1, n);
+  fin >> m;
+  for(int i = 1; i <= m; i++) {
+    fin >> t >> a >> b;
+    if(t == 1) { // query
+      fout << query_heap(root, 1, n, a, b) << '\n';
+    } else {
+      update_heap(root, 1, n, a, b);
     }
-    int lb=400;
-    int m=1+(n-1)/lb;
-    ///[1+(k-1)*lb ; k*lb] este a k-a bucata de lungime lb; 1<=k<=m
-    ///pentru fiecare bucata k precalculam minimul vmin[k]
-    for(int k=1;k<=m;k++){
-        vmin[k]=2000000000;
-        int a=1+(k-1)*lb;
-        int b=min(n,k*lb);
-        for(int i=a;i<=b;i++){
-            if(v[i]<vmin[k]){
-                vmin[k]=v[i];
-            }
-        }
-    }
-    fin>>Q;
-    for(int q=1;q<=Q;q++){
-        int t,x,y;
-        fin>>t>>x>>y;
-        if(t==2){
-            v[x]=y;
-            int k=1+(x-1)/lb;
-            ///recalculam minimul din bucata a k-a
-            vmin[k]=2000000000;
-            int a=1+(k-1)*lb;
-            int b=min(n,k*lb);
-            for(int i=a;i<=b;i++){
-                if(v[i]<vmin[k]){
-                    vmin[k]=v[i];
-                }
-            }
-        }
-        else{
-            int tmin=2000000000;
-            int kx=1+(x-1)/lb;
-            int ky=1+(y-1)/lb;
-            if(kx==ky){
-                for(int i=x;i<=y;i++){
-                    if(v[i]<tmin){
-                        tmin=v[i];
-                    }
-                }
-            }
-            else{
-                int b=min(n,kx*lb);
-                for(int i=x;i<=b;i++){
-                    if(v[i]<tmin){
-                        tmin=v[i];
-                    }
-                }
-                int a=1+(ky-1)*lb;
-                for(int i=a;i<=y;i++){
-                    if(v[i]<tmin){
-                        tmin=v[i];
-                    }
-                }
-                for(int i=kx+1;i<=ky-1;i++){
-                    if(vmin[i]<tmin){
-                        tmin=vmin[i];
-                    }
-                }
-            }
-            fout<<tmin<<"\n";
-        }
-    }
-    fin.close(); fout.close();
-    return 0;
+  }
+  return 0;
 }
