c6682978c69b08e27e7ff05fa9077f9d08ad0f13
HaibaoM55 <raulbogdan559@gmail.com>
2025-10-25 17:07:40 +0300

    25 octombrie 2025

--- matrice3/main.cpp
+++ matrice3/main.cpp
@@ -1,57 +1,67 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
-int n, m;
-int x;
-int a[103][103];
-int s[105];
-int main()
-{
-    cin >> n >> m;
+int n, m, k;
+int a[104][104];
+int s[104];
+unordered_map<int, int> f;
+int vmax = 0, vmaxi = 0;
+struct modificari{
+    int i, j, val;
+}mod[104];
+int main(){
+    cin >> n >> m >> k;
     for(int i = 1; i <= n; i++){
-        s[i] = 0;
         for(int j = 1; j <= m; j++){
             cin >> a[i][j];
             s[i] += a[i][j];
         }
-    }
-    int vmax = 0, vmaxi;
-    for(int i = 1; i <= n; i++){
-        int nr = 0;
-        for(int j = 1; j <= n; j++){
-            if(s[j] == s[i]){
-                nr++;
-            }
-        }
-        if(nr > vmax){
-            vmax = nr;
-            vmaxi = i;
+        f[s[i]]++;
+        if(f[s[i]] > f[vmax]){
+            vmax = s[i];
         }
     }
-    cout << n-vmax << '\n';
+    int p = 0;
     for(int i = 1; i <= n; i++){
-        //s[i]+x = s[vmaxi]
-        //x = s[vmaxi]-s[i];
-        if(s[i] != s[vmaxi]){
-            int vmin=1000001, vmini;
-            int vmax1=0, vmaxi1;
-            for(int j = 1; j <= m; j++){
-                if(a[i][j] > vmax1){
-                    vmax1 = a[i][j];
-                    vmaxi1 = j;
+        if(s[i] != vmax){
+            int vmax1 = 0, vmaxi=0;
+            if(vmax-s[i] < 0){
+                for(int j = 1; j <= m; j++){
+                    if(a[i][j] > vmax1){
+                        vmax1 = a[i][j];
+                        vmaxi = j;
+                    }
                 }
-                if(a[i][j] < vmin){
-                    vmin = a[i][j];
-                    vmini = j;
+                p++;
+                mod[p].i = i;
+                mod[p].j = vmaxi;
+                mod[p].val = vmax-s[i]+a[i][vmaxi];
+                if(vmax-s[i]+a[i][vmaxi] < 0){
+                    vmax1 = 0;
+                    mod[p].val = 0;
+                    int vmaxi1 = 0;
+                    for(int j = 1; j <= m; j++){
+                        if(a[i][j] > vmax1 && j != vmaxi){
+                            vmaxi1 = i;
+                            vmax1 = a[i][j];
+                        }
+                    }
+                    p++;
+                    mod[p].i = i;
+                    mod[p].j = vmaxi1;
+                    mod[p].val = vmax-s[i]+a[i][vmaxi]+a[i][vmaxi1];
                 }
-            }
-            if(s[i] > s[vmaxi]){
-                a[i][vmaxi1] -= s[i]-s[vmaxi];
-                cout << i << ' ' << vmaxi1 << ' '<<a[i][vmaxi1] << '\n';
             }else{
-                a[i][vmini] += s[vmaxi]-s[i];
-                cout << i << ' ' << vmini << ' '<<a[i][vmini] << '\n';
+                p++;
+                mod[p].i = i;
+                mod[p].j = 1;
+                mod[p].val = vmax-s[i]+a[i][1];
             }
         }
+    }
+    cout << p << '\n';
+    for(int i = 1; i <= p; i++){
+        cout << mod[i].i << ' ' << mod[i].j << ' ' << mod[i].val << '\n';
     }
     return 0;
 }
