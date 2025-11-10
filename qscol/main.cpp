17659aee0f2db4906c4da4257eb8dbd0a0284730
HaibaoM55 <raulbogdan559@gmail.com>
2025-11-10 20:30:46 +0200

    10 noiembrie 2025

--- qscol/main.cpp
+++ qscol/main.cpp
@@ -1,46 +1,29 @@
 #include <fstream>
+#include <algorithm>
 using namespace std;
 ifstream fin("qscol.in");
 ofstream fout("qscol.out");
 int n;
 int a[804][804];
-bool compara(int i, int j){
-    for(int x = 1; x <= n; x++){
-        if(a[x][i] < a[x][j]){
-            return false;
-        }else if(a[x][i] > a[x][j]){
-            return true;
-        }
-    }
-}
-bool b[804];
 int f[804];
+bool compara(int c1, int c2) {
+    for (int x = 1; x <= n; x++) {
+        if (a[x][c1] < a[x][c2]) return true;
+        if (a[x][c1] > a[x][c2]) return false;
+    }
+    return false;
+}
 int main(){
     fin >> n;
     for(int i = 1; i <= n; i++){
-        b[i] = true;
+        f[i] = i;
+    }
+    for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
             fin >> a[i][j];
         }
     }
-    for(int z = 1; z <= n; z++){
-        int vmaxi;
-        for(int i = 1; i <= n; i++){
-            if(b[i]){
-                vmaxi = i;
-                break;
-            }
-        }
-        for(int i = vmaxi+1; i <= n; i++){
-            if(b[i]){
-                if(compara(vmaxi, i)){
-                    vmaxi = i;
-                }
-            }
-        }
-        f[z] = vmaxi;
-        b[vmaxi] = false;
-    }
+    sort(f+1, f+n+1, compara);
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
             fout << a[i][f[j]] << ' ';
