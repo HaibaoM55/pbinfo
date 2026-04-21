f9758da40ed827980742cae6a9103aaf1d7510c2
HaibaoM55 <raulbogdan559@gmail.com>
2026-04-21 20:38:28 +0300

    21 aprilie 2026

---
+++ partitiinumar6/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n;
+bool exista = false;
+void kp(int x, vector<int> v, int vmax){
+    if(x == 0){
+        for(int i = 1; i <= v[0]; i++){
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+        exista = true;
+    }else{
+        v[0]++;
+        for(int i = vmax+1; i <= x; i++){
+            if(i % 2 == 1){
+                v[v[0]] = i;
+                kp(x-i, v, i);
+            }
+        }
+    }
+}
+vector<int> clearv(67);
+int main(){
+    cin >> n;
+    kp(n, clearv, 0);
+    if(!exista){
+        cout << "imposibil";
+    }
+    return 0;
+}
