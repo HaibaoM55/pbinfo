4b5a5da90e676d6e825df3d2769dbaa050fdfb0d
HaibaoM55 <raulbogdan559@gmail.com>
2026-04-29 20:46:59 +0300

    29 aprilie 2026

--- trepte2.2/main.cpp
+++ trepte2.2/main.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
 using namespace std;
+#define MOD 9001
 int n, k;
-int dp[1000004];
-void dof(int x){
-    for(int i = x+1; i <= k; i++){
-        dp[i]++;
-        dof(i);
-    }
-}
+short dp[1000004];
+short s[1000004];
 int main(){
     cin >> n >> k;
-    //cout << sizeof(dp);
+    s[0] = 0;
+    s[1] = 1;
     dp[1] = 1;
-    dp[2] = 1;
-    for(int i = 3; i <= k; i++){
-        dp[i] = dp[i-1]*2;
-        dp[i] = dp[i]%9001;
-    }
-    for(int i = k+1; i <= n; i++){
-        dp[i] = (2*dp[i-1]-dp[i-k-1]+9001)%9001;
+    for(int i = 2; i <= n; i++){
+        dp[i] = (s[i-1]-s[max(0, i-k-1)]+MOD)%MOD;
+        s[i]  = (s[i-1]+dp[i])%MOD;
     }
     cout << dp[n];
     return 0;
