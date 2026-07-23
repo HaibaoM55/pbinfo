490b329294fca247b5bf2af6d984472de4fcf6fa
HaibaoM55 <raulbogdan559@gmail.com>
2026-07-23 10:57:31 +0300

    23 iulie 2026

---
+++ grade2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int n, m;
+int x, y;
+int intr[104], extr[104];
+int main(){
+    cin >> n >> m;
+    for(int i = 1; i <= m; i++){
+        cin >> x >> y;
+        extr[x]++;
+        intr[y]++;
+    }
+    int nr = 0;
+    for(int i = 1; i <= n; i++){
+        if(intr[i] == extr[i]){
+            nr++;
+        }
+    }
+    cout << nr << '\n';
+    for(int i = 1; i <= n; i++){
+        if(intr[i] == extr[i]){
+            cout << i << ' ';
+        }
+    }
+    return 0;
+}
