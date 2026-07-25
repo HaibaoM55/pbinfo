7b16f7e89ab22c1d688594f0392ab8d85568d852
HaibaoM55 <raulbogdan559@gmail.com>
2026-07-25 10:50:44 +0300

    25 iulie 2026

---
+++ componenteconexe3/main.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ifstream fin("componenteconexe3.in");
+ofstream fout("componenteconexe3.out");
+int n, x, y;
+int nr = 0;
+bool viz[104];
+vector<int> v[104];
+vector<int> cc[104];
+void visit(int k){
+    cc[nr].push_back(k);
+    viz[k] = true;
+    int l = v[k].size();
+    for(int i = 0; i < l; i++){
+        if(!viz[v[k][i]]){
+            viz[v[k][i]] = true;
+            visit(v[k][i]);
+        }
+    }
+}
+int main(){
+    fin >> n;
+    while(fin >> x >> y){
+        v[x].push_back(y);
+        v[y].push_back(x);
+    }
+    for(int i = 1; i <= n; i++){
+        if(!viz[i]){
+            nr++;
+            visit(i);
+        }
+    }
+    int lmax = 0, lmaxi = 1;
+    for(int i = 1; i <= nr; i++){
+        sort(cc[i].begin(), cc[i].end());
+        int l = cc[i].size();
+        if(l > lmax){
+            lmax = l;
+            lmaxi = i;
+        }
+    }
+    fout << cc[lmaxi][0] << ' ' << lmax;
+    return 0;
+}
