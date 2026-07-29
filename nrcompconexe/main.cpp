6feccc3ef8f10897f1fd3fdcea0b9fd9be9efa41
HaibaoM55 <raulbogdan559@gmail.com>
2026-07-29 09:47:37 +0300

    29 iulie 2026

---
+++ nrcompconexe/main.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ifstream fin("nrcompconexe.in");
+ofstream fout("nrcompconexe.out");
+int n, m,x, y;
+int nr = 0;
+bool viz[104];
+vector<int> v[104];
+vector<int> cc[104];
+void visit(int k){
+    cc[nr].push_back(k);
+    viz[k] = true;
+    int l = v[k].size();
+    for(int i = 0; i < l; i++){
+        if(!viz[v[k][i]]){
+            viz[v[k][i]] = true;
+            visit(v[k][i]);
+        }
+    }
+}
+int main(){
+    fin >> n >> m;
+    while(fin >> x >> y){
+        v[x].push_back(y);
+        v[y].push_back(x);
+    }
+    for(int i = 1; i <= n; i++){
+        if(!viz[i]){
+            nr++;
+            visit(i);
+        }
+    }
+    fout << nr;
+    return 0;
+}
