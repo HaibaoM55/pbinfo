9e5f9b3c469c865bd8f2a3363f88d426f78e948f
HaibaoM55 <raulbogdan559@gmail.com>
2026-08-12 11:26:23 +0300

    12 august 2026

--- canonic/main.cpp
+++ canonic/main.cpp
@@ -1,89 +1,93 @@
 #include <fstream>
 #include <algorithm>
 using namespace std;
-#define SQ 720
+#define BLOC 1000
+#pragma GCC optimize("O1")
 ifstream fin("canonic.in");
 ofstream fout("canonic.out");
 int c, n;
 long long v[500004];
-int stiva[500004];
-long long bsq[SQ];
-int dr = 0;
-int z = 1;
+long long gcd[BLOC+5];
+int R[500004];
 int main(){
+    ios_base::sync_with_stdio(false);
+    fin.tie(0);
     fin >> c;
     fin >> n;
     for(int i = 1; i <= n; i++){
         fin >> v[i];
-        if(v[i] % SQ == 0){
-            z++;
-            bsq[z] = v[i];
-        }
-        bsq[z] = __gcd(v[i], bsq[z]);
     }
     if(c == 1){
-        int nr1 = 1;
+        int nr = 1;
         long long g = v[1];
         int vmax = 1;
         for(int i = 2; i <= n; i++){
             if(v[i] % g == 0){
                 g = __gcd(g, v[i]);
-                nr1++;
+                nr++;
             }else{
-                nr1 = 1;
+                nr = 1;
                 g = v[i];
             }
-            if(nr1 > vmax){
-                vmax = nr1;
+            if(nr > vmax){
+                vmax = nr;
             }
         }
         fout << vmax;
     }else{
-        /*
         long long rasp = 0;
+        int nr = 1;
+        int blocmax = 1;
         for(int i = 1; i <= n; i++){
-            for(int j = i; j <= n; j++){
-                if(v[j] % v[i] != 0){
-                    break;
-                }
-                rasp++;
+            gcd[nr] = __gcd(gcd[nr], v[i]);
+            if(i % BLOC == 0 && i != n){
+                nr++;
+                blocmax = nr;
             }
         }
-        fout << rasp;
-        */
-        long long rasp = 0;
-        v[n+1] = 1000000LL*1000000LL;
-        for(int i = 1; i <= n; i++){
-            long long g = v[i];
-            int lst = 1;
-            int fin1 = i/SQ+1;
-            bool ok = true;
-            for(int j = i; j/SQ != fin1; j++){
-                g = __gcd(g, v[j]);
-                if(g != v[j]){
-                    ok = false;
-                    break;
+        for(int i = n; i >= 1; i--){
+            if(i < n && v[i] == v[i+1]){
+                R[i] = R[i+1] + 1;
+                rasp += R[i];
+                continue;
+            }
+            int sz = 0;
+            int binc = 2e9;
+            int bi = (i-1)/BLOC + 1;
+            int capat = min(n, bi*BLOC);
+            bool ok = false;
+            for(int j = i; j <= capat; j++){
+                if(v[j] % v[i] == 0){
+                    sz++;
                 }else{
-                    rasp++;
+                    ok = true;
+                    break;
                 }
             }
-            if(ok){
-                int tr = 0;
-                for(int b = i/SQ+1; b <= z; b++){
-                    g = __gcd(g, bsq[b]);
-                    if(v[i] % g == 0){
-                        rasp += SQ;
+            if(!ok){
+                binc = bi+1;
+                for(; binc <= blocmax; binc++){
+                    if(gcd[binc] % v[i] == 0){
+                        int st = (binc-1)*BLOC + 1;
+                        int dr = min(n, binc*BLOC);
+                        sz += dr-st+1;
                     }else{
-                        tr = (b-1)*SQ+1;
                         break;
                     }
                 }
-                for(int j = tr; j <= n; j++){
-                    if(v[j] % v[i] == 0){
-                        rasp++;
+                if(binc <= blocmax){
+                    int start = (binc-1)*BLOC + 1;
+                    for(int j = start; j <= n; j++){
+                        if(v[j] % v[i] == 0){
+                            sz++;
+                        }else{
+                            break;
+                        }
                     }
                 }
             }
+            R[i] = sz;
+            rasp += sz;
         }
         fout << rasp;
     }
