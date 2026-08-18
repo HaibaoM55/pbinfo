7c516dc6e88acb58fa4085b401a01549ecab33a1
HaibaoM55 <raulbogdan559@gmail.com>
2026-08-18 11:13:21 +0300

    18 august 2026

---
+++ ctck/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m, k, x, y;
+vector<int> v[104], cc[104];
+bool f[104][104];
+bool viz[104];
+void visit(int k, int j){
+    int l = v[k].size();
+    viz[k] = true;
+    f[j][k] = true;
+    for(int i = 0; i < l; i++){
+        if(!viz[v[k][i]]){
+            viz[v[k][i]] = true;
+            visit(v[k][i], j);
+        }
+    }
+}
+bool comp(vector<int> a, vector<int> b){
+	return a[0] < b[0];
+}
+int main(){
+    cin >> n >> m >> k;
+    for(int i = 1; i <= m; i++){
+        cin >> x >> y;
+        v[x].push_back(y);
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            viz[j] = false;
+        }
+        visit(i, i);
+    }
+    for(int i = 1; i <= n; i++){
+        viz[i] = false;
+    }
+    int rasp = 0;
+    int vmax = 0;
+    for(int i = 1; i <= n; i++){
+        int corect = -1;
+        for(int j = 1; j <= rasp; j++){
+            int l = cc[j].size();
+            bool ok = true;
+            for(int k = 0; k < l; k++){
+                if(!f[cc[j][k]][i] || !f[i][cc[j][k]]){
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok){
+                corect = j;
+                break;
+            }
+        }
+        if(corect == -1){
+            rasp++;
+            cc[rasp].push_back(i);
+        }else{
+            cc[corect].push_back(i);
+        }
+    }
+    for(int i = 1; i <= rasp; i++){
+     	bool ok = false;
+        int l = cc[i].size();
+        for(int j = 0; j < l; j++){
+            if(cc[i][j] == k){
+            	ok = true;
+                break;
+            }
+        }
+        if(ok){
+            cout << l;
+            return 0;
+        }
+    }
+    return 0;
+}
