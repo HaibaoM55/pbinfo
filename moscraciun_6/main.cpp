8ea687deed41f5b11d60c1f6b83a6f33e0a0b9f9
HaibaoM55 <raulbogdan559@gmail.com>
2026-08-29 15:38:18 +0300

    29 august 2026

---
+++ moscraciun_6/main.cpp
@@ -0,0 +1,47 @@
+#include <fstream>
+using namespace std;
+ifstream fin("moscraciun_6.in");
+ofstream fout("moscraciun_6.out");
+int n, m, k, nr;
+int a[104][104];
+int dirx[6] = {0,0,1,-1};
+int diry[7] = {-1,1,0,0};
+void fill(int i, int j){
+    if(a[i][j] == 2){
+        nr++;
+    }
+    a[i][j] = 0;
+    for(int d = 0; d < 4; d++){
+        int i1 = i+dirx[d];
+        int j1 = j+diry[d];
+        if(1 <= i1 && i1 <= n){
+            if(1 <= j1 && j1 <= m){
+                if(a[i1][j1] == 1 || a[i1][j1] == 2){
+                    fill(i1, j1);
+                }
+            }
+        }
+    }
+}
+int main(){
+    fin >> n >> m >> k;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            fin >> a[i][j];
+        }
+    }
+    int rasp = 0;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(a[i][j] == 1 || a[i][j] == 2){
+                nr = 0;
+                fill(i, j);
+                if(nr >= k){
+                    rasp++;
+                }
+            }
+        }
+    }
+    fout << rasp;
+    return 0;
+}
