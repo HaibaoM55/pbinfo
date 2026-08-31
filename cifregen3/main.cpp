cdee29e1ad6d7d119dcd996b457ca8f4f337eaa1
HaibaoM55 <raulbogdan559@gmail.com>
2026-08-31 10:59:25 +0300

    31 august 2026

---
+++ cifregen3/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int n, m;
+int v[10];
+void bt(int k){
+    if(k == n+1){
+        for(int i = 1; i <= n; i++){
+            cout << v[i];
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 0; i < m; i++){
+     	if(i == 0 && k == 1){
+            continue;
+     	}
+        if(k == n){
+            if(i != v[1]){
+            	continue;
+            }
+		}
+	    v[k] = i;
+        bt(k+1);
+    }
+}
+int main(){
+    cin >> n >> m;
+    if(n == 1){
+        for(int i = 0; i < m; i++){
+        	cout << i << '\n';
+        }
+        return 0;
+    }
+    bt(1);
+    return 0;
+}
